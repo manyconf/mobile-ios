754471f71e00ee08570209878f9fb4a8d03d761a
Bart van Kuik <bart@vankuik.nl>
2014-11-21 12:01:46 +0100

    Added missing settings/spotlight images

--- assets/create_ios_icons.sh
+++ assets/create_ios_icons.sh
@@ -13,11 +13,13 @@ src_dir="../../mobile-assets/${theme}"
 # Image sizes as defined by Apple
 app_icon="app_icon.png"
 app_icon_sizes="180 120 152 76"
+spotlight_icon_sizes="29 58"
+settings_icon_sizes="40 80 120"
 app_store_icons="512 1024"
 launch_icons="640x1136 640x960 320x480 1536x2008 768x1004"
 spotlight_icons="80 40"
 
-for i in $app_icon_sizes; do
+for i in $app_icon_sizes $settings_icon_sizes $spotlight_icon_sizes; do
     echo "Resizing $app_icon to $i x $i"
     convert -resize $i $src_dir/$app_icon app_icon_${i}.png
 done
